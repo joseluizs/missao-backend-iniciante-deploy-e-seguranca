d80e48a8394ebd9b626247c79883fac4e5371781
José Luiz <36302287+joseluizs@users.noreply.github.com>
2024-09-23 12:55:55 +0000

    Configuração do ´dotenv´

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const { MongoClient, ObjectId } = require('mongodb')
 
 //Preparamos as informações de acesso ao banco de dados
-const dbUrl = 'mongodb+srv://admin:6LC5yrm08fIhewO3@cluster0.2je23.mongodb.net'
+const dbUrl = process.env.DATABASE_URL
 const dbName = 'mongodb-intro-e-implementacao'
 
 //Declaramos a função main
